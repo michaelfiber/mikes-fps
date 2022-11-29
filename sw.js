d1c334f51b414d7ea3d075c478b7fba6f4361b44
Action: Push to Branch <action@github.com>
2022-11-29 04:03:35 +0000

    Update gh-pages to output generated at

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "RAYLIB_GAME_TEMPLATE_FORK_CACHE_NAME";
+const cacheName = "cache_name_1669694613";
 
 self.addEventListener('install', (event) => {
 	event.waitUntil(caches.open(cacheName));
